09ec4c61a2444a6082afb37d590efdb5305e91e1
Richard Lynch <rich753@hotmail.com>
2017-11-08 18:48:39 +0000

    Added demo

--- main.py
+++ main.py
@@ -3,11 +3,27 @@
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
+langs=[
+        {'name':'Python', 'pros' : 'easy', 'cons' : 'high-level'}, 
+        {'name':'C++', 'pros' : 'fast', 'cons' : 'hard'}, 
+        {'name':'java', 'pros': 'best of both', 'cons' : 'stoopid'}
+        ]
+
 @app.route('/', methods=['GET'])
 def test():
     return jsonify({'message':'It Works!'})
-
-
+@app.route('/langs', methods=['GET'])
+def returnlangs():
+    return jsonify({'langs' : langs})
+@app.route('/langs/<string:name>', methods=['GET'])
+def returnlang(name):
+    return jsonify({'lang': [lang for lang in langs if lang['name'] == name][0] })
+@app.route('/langs', methods=['POST'])
+def addlang():
+    lang = { 'name' : request.json['name'], 'pros' : request.json['pros'], 'cons' : request.json['cons'] }
+    if len([tlang for tlang in langs if tlang['name'] == lang['name']]) == 0:
+        langs.append(lang)
+    return jsonify({'langs' : langs})
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
 
