e6fcd87598a8175699328560d5b06b32be311de8
Richard Lynch <rich753@hotmail.com>
2017-11-09 13:10:55 +0000

    Added rget.sh

---
+++ rget.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+command="79.97.206.77:8080/"
+if [[ $# > 0 ]] ; then
+    echo "greater than 1"
+    command="$command$1"
+    echo $command
+fi
+if [[ $# > 1 ]] ; then
+    echo "greater than 2"
+    command="$command/$2"
+    echo $command
+fi
+if [[ $# > 2 ]] ;  then
+    echo "greater then 3"
+    command="$command/$3"
+    echo $command
+fi
+
+curl $command 
